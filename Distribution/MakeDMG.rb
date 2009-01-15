ac60fed6757c856cb921f3bad6a83b3fb529c94e
Tim Morgan <riscfuture@gmail.com>
2009-01-14 16:50:14 -0800

    Added new distribution script to be run in the command line after the build is complete.
    Run Script build phases apparently only run before the file is stripped, so we have to use the command line in order to compress things as much as possible. Also, we got custom icons and background images for our disk image now.

---
+++ Distribution/MakeDMG.rb
@@ -0,0 +1,37 @@
+#!/usr/bin/env ruby
+
+product = "Scribd\\ Uploader"
+dist_dir = "build/Install/dist"
+app = "#{dist_dir}/#{product}.app"
+sla = "Distribution/SLA.r"
+vol_template = "Distribution/Volume\\ Template"
+big_dmg = "#{dist_dir}/#{product}-tmp.dmg"
+dmg = "#{dist_dir}/#{product}.dmg"
+
+`rm -f #{big_dmg}`
+`rm -f #{dmg}`
+
+mount_info = `hdiutil create -size 10m -volname #{product} -fs HFS+ -attach -uid 99 -gid 99 #{big_dmg}`
+drive_ident = mount_info.match(/^(\/dev\/.+?)\s+/m)[1]
+mountpoint = mount_info.match(/^.+?\s+.+?\s+(\/Volumes\/.+?)$/m)[1]
+mountpoint.gsub! ' ', '\\ '
+
+`cp -R #{app} #{mountpoint}/#{product}.app`
+`mkdir #{mountpoint}/.background`
+`cp #{vol_template}/Background.png #{mountpoint}/.background/dmgBackground.png`
+`cp #{vol_template}/Volume\\ Icon.icns #{mountpoint}/.VolumeIcon.icns`
+`cp #{vol_template}/DS_Store #{mountpoint}/.DS_Store`
+
+`SetFile -a C #{mountpoint}`
+
+`ln -s /Applications #{mountpoint}/Applications`
+
+`hdiutil detach #{mountpoint}`
+`hdiutil convert #{big_dmg} -format UDZO -o #{dmg}`
+
+`hdiutil unflatten #{dmg}`
+`Rez /Developer/Headers/FlatCarbon/*.r #{sla} -a -o #{dmg}`
+`hdiutil flatten #{dmg}`
+
+`hdiutil internet-enable -yes #{dmg}`
+`rm -f #{big_dmg}`
