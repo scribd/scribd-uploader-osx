ccf072b174e9cc09635413011c2e2a7ca19c4493
Tim Morgan <riscfuture@gmail.com>
2009-03-20 02:15:22 -0700

    Asynchronous conversion status updates

--- Classes/Controller/Helpers/SUConversionStatusTracker.h
+++ Classes/Controller/Helpers/SUConversionStatusTracker.h
@@ -1,8 +1,39 @@
+/*!
+ @class SUConversionStatusTracker
+ @abstract Asynchronously and periodically polls Scribd.com for the conversion
+ status of a document, updating its converting attribute as appropriate.
+ @discussion The conversion tracking begins as soon as an instance is created
+ with a document. This class should be autoreleased to ensure it doesn't go out
+ of scope.
+ 
+ This class sends a request to Scribd.com every second. Multiple simultaneous
+ instances do not throttle their requests.
+ */
+
 @interface SUConversionStatusTracker : NSObject {
 	SUDocument *document;
+	NSTimer *timer;
 }
 
+#pragma mark Properties
+
+/*!
+ @property document
+ @abstract The document whose conversion status this instance is tracking and
+ updating.
+ */
+
 @property (retain) SUDocument *document;
+
+#pragma mark Initializing and deallocating
+
+/*!
+ @property initWithDocument:
+ @abstract Creates a new instance that tracks a given document.
+ @param doc The document to track.
+ @result The initialized instance.
+ @discussion This is the designated initializer.
+ */
 
 - (id) initWithDocument:(SUDocument *)doc;
 
--- Classes/Controller/Helpers/SUConversionStatusTracker.m
+++ Classes/Controller/Helpers/SUConversionStatusTracker.m
@@ -2,37 +2,68 @@
 
 @implementation SUConversionStatusTracker
 
+#pragma mark Properties
+
 @synthesize document;
+
+#pragma mark Initializing and deallocating
 
 - (id) initWithDocument:(SUDocument *)doc {
 	if (self = [super init]) {
 		self.document = doc;
-		NSTimer *timer = [NSTimer timerWithTimeInterval:2.0 target:self selector:@selector(update:) userInfo:NULL repeats:YES];
+		timer = [NSTimer timerWithTimeInterval:2.0 target:self selector:@selector(update:) userInfo:NULL repeats:YES];
 		[[NSRunLoop currentRunLoop] addTimer:timer forMode:NSDefaultRunLoopMode];
 	}
 	return self;
 }
+
+/*
+ Releases local memory usage.
+ */
 
 - (void) dealloc {
 	if (document) [document release];
 	[super dealloc];
 }
 
-- (void) update:(NSTimer *)timer {
-	NSError *error = NULL;
+#pragma mark Periodic updates
+
+/*
+ Called regularly, begins an asynchronous API request for the conversion status.
+ */
+
+- (void) update:(NSTimer *)sendingTimer {
 	NSDictionary *parameters = [[NSDictionary alloc] initWithObjectsAndKeys:
 								document.scribdID, @"doc_id",
 								[SUSessionHelper sessionHelper].key, @"session_key",
 								NULL];
-	NSDictionary *result = [[SUScribdAPI sharedAPI] callAPIMethod:@"docs.getConversionStatus" parameters:parameters error:&error];
+	[[SUScribdAPI sharedAPI] asynchronouslyCallAPIMethod:@"docs.getConversionStatus" parameters:parameters delegate:self];
+	//TODO throttle these requests if there are a lot of instances
 	[parameters release];
-	NSString *status = [result objectForKey:@"conversion_status"];
-	if (error || !status) return;
-	
-	if ([status isEqualToString:@"PROCESSING"]) document.converting = [NSNumber numberWithBool:YES];
-	else {
-		document.converting = [NSNumber numberWithBool:NO];
-		[timer invalidate];
+}
+
+#pragma mark ASIHTTPRequest delegate methods
+
+/*
+ Parses the server's response and updates the document's attributes as
+ appropriate. Kills the timer if conversion is complete.
+ */
+
+- (void) requestFinished:(ASIHTTPRequest *)request {
+	NSError *error = NULL;
+	NSXMLDocument *xml = [[NSXMLDocument alloc] initWithXMLString:[request responseString] options:0 error:&error];
+	if (xml) {
+		NSDictionary *response = [[SUScribdAPI sharedAPI] parseXML:xml error:&error];
+		//TODO shouldn't use a private method here
+		if (response) {
+			NSString *status = [response objectForKey:@"conversion_status"];
+			if (!status) return;
+			if ([status isEqualToString:@"PROCESSING"]) document.converting = [NSNumber numberWithBool:YES];
+			else {
+				document.converting = [NSNumber numberWithBool:NO];
+				[timer invalidate];
+			}
+		}
 	}
 }
 
